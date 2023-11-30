0de664eb1a93ad2a4cf69986629ef3590b8fec84
elteeestoo <deiwid22david@gmail.com>
2023-11-30 15:49:49 +0100

    validation

--- src/form-validation.js
+++ src/form-validation.js
@@ -1,10 +1,17 @@
 export default (() => {
     
     let input = document.querySelector(".validate");
-    let minlenght = input.dataset.minlenght;
+    let minlenght = input.dataset.minlength;
     
 input.addEventListener("input" , () => {
-    if (input.value.length < parseInt(minlenght) && input.value.length > 0)
-})
+    console.log(input.value.length)
+    if (input.value.length < minlenght) {
+        input.classList.add("input-active");
+    }
 
-})();
+    if (input.value.length >= minlenght) {
+        input.classList.remove("input-active");
+    }
+    
+    if (input.value != )
+})})();
--- src/windows.js
+++ src/windows.js
@@ -1,25 +1,21 @@
 export default (() => {
 
-    const tabsSection = document.querySelector(".tabs");
-
-    tabsSection.addEventListener("click", (event) => {
-
-        if(event.target.closest('.tab')){
-            const tab = event.target.closest('.tab');
-            tab.parentElement.querySelector('.active').classList.remove('active');
-            tab.classList.add('active');
-
-            const tabContents = document.querySelectorAll('.tab-content');
-
-            tabContents.forEach(tabContent => {
-                if(tab.dataset.tab == tabContent.dataset.tab ){
-                    tabContent.classList.add('active');
-                }else{
-                    tabContent.classList.remove('active');
-                }
-            })
-        }
-    });
-})();
-
+    const data = document.querySelector(".form-data");
+    const databutton = document.querySelector(".form-data button");
+    const images = document.querySelector(".form-images");
+    const imagesbutton = document.querySelector(".form-images button");
   
+    
+    databutton?.addEventListener("click", () => {
+        data.classList.add("active");
+        images.classList.remove("active");
+
+    });
+    imagesbutton?.addEventListener("click", () => {
+        images.classList.add("active");
+        data.classList.remove("active");
+        imagesbutton.classList.toggle("active");
+      });
+  
+  })();
+    
